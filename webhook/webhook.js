7f5b660e641181b26544ff41b08270ef36e699e0
yiyanglin0102 <yiyanglin0102@gmail.com>
2022-04-27 00:58:11 -0500

    Create adding products with number to Cart

--- webhook/webhook.js
+++ webhook/webhook.js
@@ -18,6 +18,26 @@ if (USE_LOCAL_ENDPOINT) {
   ENDPOINT_URL = "https://cs571.cs.wisc.edu";
 }
 
+async function postBack(isBack) {
+  let obj = {}
+  obj.back = isBack;
+  let request = {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-access-token': token
+    },
+    body: JSON.stringify(obj),
+    redirect: 'follow'
+  }
+
+  const serverReturn = await fetch('https://cs571.cs.wisc.edu/application', request)
+  const serverResponse = await serverReturn.json()
+  console.log(serverResponse);
+
+  return serverResponse;
+
+}
 
 async function agentMessage(message) {
   let obj = {}
@@ -77,6 +97,44 @@ async function getCart() {
   console.log(serverResponse);
   return serverResponse;
 }
+
+async function clearDeleteCart() {
+  let request = {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-access-token': token
+    },
+    redirect: 'follow'
+  }
+
+  const serverReturn = await fetch('https://cs571.cs.wisc.edu/application/products', request);
+  const serverResponse = await serverReturn.json();
+  console.log(serverResponse);
+  return serverResponse.message;
+}
+
+async function productNameToID(name) {
+  let request = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+    redirect: 'follow'
+  }
+
+  const serverReturn = await fetch('https://cs571.cs.wisc.edu/products', request)
+  const serverResponse = await serverReturn.json()
+  // console.log(serverResponse.products);
+
+  for (let i = 0; i < serverResponse.products.length; i++) {
+    if (serverResponse.products[i].name === name.replaceAll('_', ' ')) {
+      console.log(serverResponse.products[i].id);
+      return serverResponse.products[i].id;
+    }
+
+  }
+  return null;
+}
+
 async function getProducts(productName) {
   let request = {
     method: 'GET',
@@ -152,6 +210,61 @@ async function deleteMessage() {
   console.log(serverResponse);
   return serverResponse;
 }
+
+async function getPage() {
+  let request = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-access-token': token
+    },
+    redirect: 'follow'
+  }
+
+  const serverReturn = await fetch('https://cs571.cs.wisc.edu/application', request)
+  const serverResponse = await serverReturn.json()
+  console.log(serverResponse);
+
+  return serverResponse;
+}
+
+async function postPage(page) {
+  console.log(page);
+  let obj = {}
+  obj.page = page;
+  let request = {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-access-token': token
+    },
+    body: JSON.stringify(obj),
+    redirect: 'follow'
+  }
+
+  const serverReturn = await fetch('https://cs571.cs.wisc.edu/application', request)
+  const serverResponse = await serverReturn.json()
+
+  return serverResponse;
+}
+
+async function addProductToCart(id) {
+  let obj = {}
+  let request = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-access-token': token
+    },
+    body: JSON.stringify(obj),
+    redirect: 'follow'
+  }
+  const serverReturn = await fetch('https://cs571.cs.wisc.edu/application/products/' + id, request)
+  const serverResponse = await serverReturn.json()
+
+  return serverResponse;
+}
+
 app.get("/", (req, res) => res.send("online"));
 app.post("/", express.json(), (req, res) => {
   const agent = new WebhookClient({ request: req, response: res });
@@ -174,6 +287,14 @@ app.post("/", express.json(), (req, res) => {
       return agent.add("Sorry your password or username may not be correct.")
     }
     await userMessage(username);
+
+    let encryptPassword = "";
+    for (let i = 0; i < password.length; i++) {
+      encryptPassword += "*";
+    }
+
+    await userMessage(encryptPassword);
+
 
     await agentMessage("Welcome " + username + " !!!")
     agent.add("Welcome " + username + " !!!")
@@ -203,12 +324,47 @@ app.post("/", express.json(), (req, res) => {
       cartString = cartString.substring(0, cartString.length - 2);
       cartString = "You have added " + numItem + " items to the cart which are " + totalPrice + " dollars. " + "The detail product list are: " + cartString + ".";
       agent.add(cartString);
-      await agentMessage(cartString);
     }
 
   }
 
+  async function clearCart() {
+    await userMessage(agent.query);
+    await getToken();
+    if (token === "" || typeof token === 'undefined') {
+      await agentMessage("Sorry I cannot do it. Can you log in?");
+      return agent.add("Sorry I cannot do it. Can you log in?");
+    }
+    let msg = await clearDeleteCart();
+    await agentMessage(msg);
+    agent.add(msg);
+  }
+
+  async function addToCart() {
+
+    let addNumber = agent.parameters.addNumber;
+    let name = agent.parameters.Products;
+
+    console.log(name);
+    // function - search the name to matched ID 
+    let id = await productNameToID(name);
+
+    await userMessage(agent.query);
+    await getToken();
+    if (token === "" || typeof token === 'undefined') {
+      await agentMessage("Sorry I cannot do it. Can you log in?");
+      return agent.add("Sorry I cannot do it. Can you log in?");
+    }
+
+    for (let i = 0; i < addNumber; i++) {
+      await addProductToCart(id);
+    }
+    await agentMessage("added " + addNumber + " " + name.replaceAll('_', ' ') + " to cart.");
+    agent.add("added " + addNumber + " " + name.replaceAll('_', ' ') + " to cart.");
+  }
+
   async function checkProducts() {
+    await userMessage(agent.query)
     productName = agent.parameters.productName;
     let productList = await getProducts(productName);
     let productString = "";
@@ -220,10 +376,11 @@ app.post("/", express.json(), (req, res) => {
     }
     productString = productString.slice(0, productString.length - 2);
     agent.add(productString);
+    await agentMessage(productString);
   }
 
   async function checkCategory() {
-    await userMessage(agent.query)
+    await userMessage(agent.query);
     let categoryList = await getCategory();
     categoryList = categoryList.categories;
     let categoryString = "We have the following products: ";
@@ -253,6 +410,36 @@ app.post("/", express.json(), (req, res) => {
 
   }
 
+  async function goBack() {
+    await getToken();
+    if (token === "" || typeof token === 'undefined') {
+      await agentMessage("Sorry I cannot do it. Can you log in?");
+      return agent.add("Sorry I cannot do it. Can you log in?");
+    }
+
+    await postBack(true);
+    await agentMessage("You are now viewing the previous page");
+    agent.add("You are now viewing the previous page");
+  }
+
+  async function goPage() {
+    let intendPage = agent.parameters.intendPage;
+    await userMessage(agent.query)
+    if (intendPage === "Sign Up") {
+      await postPage("/signUp")
+      await agentMessage("You are now view the sign up page of WiscShop!")
+      agent.add("You are now view the sign up page of WiscShop!")
+    }
+    // if (userPage === "Sign Up") {
+    //   await postPage("/signUp")
+    //   await agentMessage("You are now view the sign up page of WiscShop!")
+    //   agent.add("You are now view the sign up page of WiscShop!")
+    // }
+    else
+      agent.add(intendPage);
+
+  }
+
   let intentMap = new Map();
   intentMap.set("Default Welcome Intent", welcome);
   // You will need to declare this `Login` intent in DialogFlow to make this work
@@ -260,7 +447,11 @@ app.post("/", express.json(), (req, res) => {
   intentMap.set('GetCategory', checkCategory);
   intentMap.set('GetTags', checkTags);
   intentMap.set('GetCart', checkCart);
+  intentMap.set('ClearCart', clearCart);
+  intentMap.set('AddCart', addToCart);
   intentMap.set('GetProducts', checkProducts);
+  intentMap.set('GoBack', goBack);
+  intentMap.set('GoPage', goPage);
 
   agent.handleRequest(intentMap);
 });
